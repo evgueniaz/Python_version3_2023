a1cc25f8717a585216ec9df8da7221ee45ad71dc
Evguenia Zanina <evgueniaz@hotmail.com>
2023-04-30 00:41:10 +0300

    HW 5 Ex 28

--- HW5/HW5_Task28.py
+++ HW5/HW5_Task28.py
@@ -2,7 +2,7 @@
 # Из всех арифметических операций допускаются только +1 и -1. Также нельзя использовать циклы.
 
 
-def adding_two_nums(num_1, num_2):
+def adding_two_nums(num_1: int, num_2: int) -> int:
     if num_2 == 0 or (num_1 == 0 and num_2 == 0):
         return num_1
     return adding_two_nums(num_1 + 1, num_2 - 1)
